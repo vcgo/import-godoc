9270dc3326c409336752e68cb5a9d16bd35b9477
Weilong Wang <wilonx@163.com>
2018-09-13 15:48:01 +0800

    Update import_godoc.go

--- import_godoc.go
+++ import_godoc.go
@@ -4,15 +4,6 @@ import (
     "github.com/vcgo/kit"
     "github.com/vcgo/filecache"
     "github.com/vcgo/packagetest"
-
-    "github.com/dnfgo/wegame"
-    "github.com/dnfgo/plat"
-    "github.com/dnfgo/key"
-    "github.com/dnfgo/imgstr"
-    "github.com/dnfgo/clt"
-    "github.com/dnfgo/asura"
-    "github.com/dnfgo/move"
-    "github.com/dnfgo/num"
 )
 
 func Test() {
